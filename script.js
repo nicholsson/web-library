6fe17a41eb721002d92f1452bb06823216b2f751
nicholsson <nicoanta95@gmail.com>
2024-06-03 16:45:54 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,21 @@
+console.log("hello");
+const myLibrary = [];
+
+function Book(title, author, pages, readStatus){
+
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.readStatus = readStatus;
+}
+
+function addBookToLibrary(){
+    //takes data from input
+    const title = document.querySelector("#title").value;
+    const author = document.querySelector("#author").value;
+    const pages = document.querySelector("#pages").value;
+    const readStatus = document.querySelector('input[name="readStatus"]:checked').value;
+    //add book to library
+    const newBook = new Book(title, author, pages, readStatus);
+    myLibrary.push(newBook);
+}
