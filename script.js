4023f2a50fcb1baf8826b2fa7429beaec5d96397
Nicola <nicoanta95@gmail.com>
2024-06-10 20:18:12 +0200

    add functionality to delete and toggle status buttons

--- script.js
+++ script.js
@@ -3,11 +3,27 @@ const addButton = document.querySelector(".add-book");
 const addBook = document.querySelector(".add-book-to-library")
 const dialog = document.querySelector("dialog");
 const closeBtn = document.querySelector("#close-btn");
+const booksContainer = document.querySelector(".books-container");
+
+booksContainer.addEventListener("click", (event) => {
+    if(event.target.classList.contains("delete-book")){
+        const bookIndex = Number(event.target.dataset.booknumber);
+        myLibrary.splice(bookIndex, 1);
+        readLibrary();
+    }
+    if(event.target.classList.contains("changeStatus")){
+        const bookIndex = Number(event.target.dataset.booknumber);
+        myLibrary[bookIndex].toggleReadStatus();
+        readLibrary();
+    }
+})
 
 let myLibrary = [];
 
 addBook.addEventListener("click", () => {
     addBookToLibrary();
+    readLibrary();
+    clearForm();
 })
 
 addButton.addEventListener("click", () => {
@@ -25,28 +41,52 @@ function Book(title, author, pages, readStatus){
     this.readStatus = readStatus;
 }
 
+Book.prototype.toggleReadStatus = function(){
+    this.readStatus = !this.readStatus;
+}
+
 function addBookToLibrary(){
     //takes data from input
     const title = document.querySelector("#title").value;
     const author = document.querySelector("#author").value;
     const pages = document.querySelector("#pages").value;
-    const readStatus = document.querySelector('input[name="readStatus"]:checked').value;
+    const readStatus = document.querySelector('#read').checked;
     //add book to library
     const newBook = new Book(title, author, pages, readStatus);
     myLibrary.push(newBook);
 }
 
 function readLibrary() {
-    for (let i = 0; i < myLibrary.length(); i++){
+    booksContainer.innerHTML = "";
+    for (let i = 0; i < myLibrary.length; i++){
         let myBook = myLibrary[i];
         let title = myBook.title;
         let pages = myBook.pages;
         let author = myBook.author;
         let readStatus = myBook.readStatus;
-        showLibrary(title, author, pages, readStatus);
+        showLibrary(title, author, pages, readStatus, i);
     }
 }
 
-function showLibrary(title, author, pages, status){
+function showLibrary(title, author, pages, status, i){
     // do something to show the library in the DOM
+    let readingStatus = "";
+    status ? readingStatus = "Read" : readingStatus = "Not read"
+    const book = document.createElement("div");
+    book.setAttribute("class", "book");
+    book.innerHTML = `<p class="book-title">${title}</p>
+    <p class="by-author">by</p>
+    <p class="author-name">${author}</p>
+    <p class="book-pages">${pages} pages</p>
+    <p class="read-status">${readingStatus}</p>
+    <button class="changeStatus" data-booknumber="${i}">Change Status</button>
+    <button class="delete-book" data-booknumber="${i}">Delete</button>`;
+    booksContainer.appendChild(book);
+}
+
+function clearForm(){
+    document.querySelector("#title").value = '';
+    document.querySelector("#author").value = '';
+    document.querySelector("#pages").value = '';
+    document.querySelector("#read").checked = false;
 }
